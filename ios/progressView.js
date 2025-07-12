e55da70b3cd2768e3c4344471b5331185f827bea
Cristian Rojas <cristian.rojas@live.fr>
2025-07-12 17:38:52 +0200

    Calculate transform and animation delay dynamically

--- ios/progressView.js
+++ ios/progressView.js
@@ -2,7 +2,7 @@ import { styledTags } from '../vanjs/styled.js'
 const { div, style } = styledTags
 
 export const iOSProgressView = () => {
-    const blade = () => {
+    const blade = (n) => {
         let view = div()
             .position('absolute')
             .top('6.5px')
@@ -13,18 +13,20 @@ export const iOSProgressView = () => {
             .borderRadius('1.25px')
             .animation('iSpinnerBlade 1s linear infinite')
             .willChange('opacity')
+            .transform(`rotate(${n*45}deg) translateY(-6.5px)`)
+            .animationDelay(`-${1.75-(0.125*n)}s`)
         return view
     }
 
     let view = div(
-        blade().transform('rotate(45deg) translateY(-6.5px)').animationDelay('-1.625s'),
-        blade().transform('rotate(90deg) translateY(-6.5px)').animationDelay('-1.5s'),
-        blade().transform('rotate(135deg) translateY(-6.5px)').animationDelay('-1.375s'),
-        blade().transform('rotate(180deg) translateY(-6.5px)').animationDelay('-1.25s'),
-        blade().transform('rotate(225deg) translateY(-6.5px)').animationDelay('-1.125s'),
-        blade().transform('rotate(270deg) translateY(-6.5px)').animationDelay('-1s'),
-        blade().transform('rotate(315deg) translateY(-6.5px)').animationDelay('-0.875s'),
-        blade().transform('rotate(360deg) translateY(-6.5px)').animationDelay('-0.75s'),
+        blade(1),
+        blade(2),
+        blade(3),
+        blade(4),
+        blade(5),
+        blade(6),
+        blade(7),
+        blade(8),
         style(`
 @keyframes iSpinnerBlade {
 0% {
