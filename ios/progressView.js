b2e7c3e40e948a3d68ff8e6a57031ff613513a66
Cristian Rojas <cristian.rojas@live.fr>
2025-07-12 18:57:21 +0200

    Add scoped animation id on progress view component

--- ios/progressView.js
+++ ios/progressView.js
@@ -1,14 +1,18 @@
 import { styledTags } from '../vanjs/styled.js'
 const { div, style } = styledTags
 
+const uid = () => crypto.randomUUID().slice(0, 8);
 export const iOSProgressView = () => {
-    return div(...blades, keyFrames())
+    const animationId = uid();
+    const blades = Array.from({ length: 8 }, (_, i) => blade(i + 1, animationId))
+
+    return div(...blades, keyFrames(animationId))
     .position('relative')
     .width('20px')
     .height('20px')
 }
 
-const blade = (n) => {
+const blade = (n, animationId) => {
     let view = div()
         .position('absolute')
         .top('6.5px')
@@ -17,18 +21,16 @@ const blade = (n) => {
         .height('6.5px')
         .backgroundColor('#8e8e93')
         .borderRadius('1.25px')
-        .animation('iSpinnerBlade 1s linear infinite')
+        .animation(`${animationId} 1s linear infinite`)
         .willChange('opacity')
         .transform(`rotate(${n*45}deg) translateY(-6.5px)`)
         .animationDelay(`-${1.75-(0.125*n)}s`)
     return view
 }
 
-const blades = Array.from({ length: 8 }, (_, i) => blade(i + 1))
-
-const keyFrames = () => {
+const keyFrames = (animationId) => {
    return style(`
-@keyframes iSpinnerBlade {
+@keyframes ${animationId} {
 0% {
 opacity: 0.85; }
 50% {
